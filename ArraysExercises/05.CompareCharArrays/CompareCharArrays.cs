cbf4af00580432b0269b1247ca87281d52614c2b
Svetoslav Stankov <sv.stankoff@gmail.com>
2018-01-28 11:26:49 +0200

    Arrays exercises

---
+++ ArraysExercises/05.CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,68 @@
+﻿namespace _05.CompareCharArrays
+{
+    using System;
+    using System.Linq;
+
+    public class CompareCharArrays
+    {
+        public static void Main()
+        {
+            char[] firstArr = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] secondArr = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+            int minLenght = Math.Min(firstArr.Length, secondArr.Length);
+
+            for (int i = 0; i < minLenght; i++)
+            {
+                if (firstArr[i] == secondArr[i])
+                {
+                    if (firstArr.Length <= secondArr.Length)
+                    {
+                        Console.WriteLine(string.Join("", firstArr));
+                        Console.WriteLine(string.Join("", secondArr));
+                        break;
+                    }
+                    else if (firstArr.Length > secondArr.Length)
+                    {
+                        Console.WriteLine(string.Join("", secondArr));
+                        Console.WriteLine(string.Join("", firstArr));
+                        break;
+                    }                    
+                }
+                else
+                {
+                    if (firstArr[i] < secondArr[i])
+                    {
+                        if (firstArr.Length <= secondArr.Length)
+                        {
+                            Console.WriteLine(string.Join("", firstArr));
+                            Console.WriteLine(string.Join("", secondArr));
+                            break;
+                        }
+                        else 
+                        {
+                            Console.WriteLine(string.Join("", secondArr));
+                            Console.WriteLine(string.Join("", firstArr));
+                            break;
+                        }                        
+                    }
+                    else
+                    {
+                        if (firstArr.Length < secondArr.Length)
+                        {
+                            Console.WriteLine(string.Join("", firstArr));
+                            Console.WriteLine(string.Join("", secondArr));
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Join("", secondArr));
+                            Console.WriteLine(string.Join("", firstArr));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
