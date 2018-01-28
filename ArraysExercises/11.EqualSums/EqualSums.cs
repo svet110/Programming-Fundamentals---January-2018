6ffc697bf0c18b37257ce0d2213b4ad736e6f4e8
Svetoslav Stankov <sv.stankoff@gmail.com>
2018-01-28 11:27:22 +0200

    Arrays exercises

---
+++ ArraysExercises/11.EqualSums/EqualSums.cs
@@ -0,0 +1,36 @@
+﻿namespace _11.EqualSums
+{
+    using System;
+    using System.Linq;
+
+    public class EqualSums
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            bool isEqual = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int[] leftSide = numbers.Take(i).ToArray();
+                int[] rightSide = numbers.Skip(i + 1).ToArray();
+
+                if (leftSide.Sum() == rightSide.Sum())
+                {
+                    Console.WriteLine(i);
+                    isEqual = true;
+                    break;
+                }
+            }
+
+            if (!isEqual)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
