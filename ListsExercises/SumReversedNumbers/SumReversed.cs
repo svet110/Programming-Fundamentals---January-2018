644ed03b7fdd3f260d2fea97b38b4e9ef595d215
Svetoslav Stankov <sv.stankoff@gmail.com>
2018-02-06 17:56:25 +0200

    ListsExercises

---
+++ ListsExercises/SumReversedNumbers/SumReversed.cs
@@ -0,0 +1,33 @@
+﻿namespace SumReversedNumbers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class SumReversed
+    {
+        public static void Main()
+        {
+            List<string> numsAsStrings = Console.ReadLine().Split(' ').ToList();
+            List<int> reversed = new List<int>();
+
+            for (int i = 0; i < numsAsStrings.Count; i++)
+            {
+                string number = numsAsStrings[i];
+                string reversedStr = reverseTheString(number);
+                int num = int.Parse(reversedStr);
+                reversed.Add(num);
+            }
+            Console.WriteLine(reversed.Sum());
+        }
+
+        public static string reverseTheString(string number)
+        {
+            char[] arr = number.ToCharArray();
+            char[] reversed = arr.Reverse().ToArray();
+            string reversedString = string.Join("", reversed);
+            return reversedString;
+        }
+    }
+    }
+}
