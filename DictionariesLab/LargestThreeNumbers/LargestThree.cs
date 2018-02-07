7649845ef3037536d98da2776d2c54ad0e42ce8f
Svetoslav Stankov <sv.stankoff@gmail.com>
2018-02-07 21:21:26 +0200

    DictionariesLab

---
+++ DictionariesLab/LargestThreeNumbers/LargestThree.cs
@@ -0,0 +1,20 @@
+﻿namespace LargestThreeNumbers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class LargestThree
+    {
+        public static void Main()
+        {
+            var list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            list.Sort();
+            list.Reverse();
+
+            list = list.Take(3).ToList();
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
---
+++ DictionariesLab/ShortWordsSorted/WordsSorted.cs
@@ -0,0 +1,33 @@
+﻿namespace ShortWordsSorted
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class WordsSorted
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine().ToLower()
+                .Split(new[] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' }
+                ,StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            var shortWords = new List<string>();
+
+            foreach (var word in text)
+            {
+                if (word.Length < 5)
+                {
+                    shortWords.Add(word);
+                }
+            }
+
+            var result = shortWords
+                .OrderBy(w => w)
+                .Distinct();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
